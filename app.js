c53d0cd219e70c88e807239a014a36b53fcff8a8
ben <ben.huang@haowin.org>
2017-09-22 19:03:22 +0800

    update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '127.0.0.1'; // 域名
+const port = 3000; // 端口
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+    res.end('Hello World Node.js\n');
+}); // 创建一个server
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+    console.log(`Server running at http://${hostname}:${port}/`); // 控制台输出
+}); // 监听端口
